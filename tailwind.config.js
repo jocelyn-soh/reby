3bb275c2498fcba09b0c9767b106ff109773f5d0
Terri <122513546+territxl@users.noreply.github.com>
2023-05-28 22:32:33 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        buttonColor: '#B0AB9E',
+        buttonHoverColor: '#CDCAC2',
+        headerColor: '#F7F4E6',
+      },
+    },
+    fontFamily: {
+      raleway: ['Raleway', 'sans-serif'],
+    }
   },
   plugins: [],
 }
