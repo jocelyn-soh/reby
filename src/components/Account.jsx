12fc3358d6406aadf9b393cc29ce63af91026d51
Terri <122513546+territxl@users.noreply.github.com>
2023-05-28 22:38:41 +0800

    Update Account.jsx

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -17,14 +17,31 @@ const Account = () => {
   };
 
   return (
-    <div className='max-w-[600px] mx-auto my-16 p-4'>
-      <h1 className='text-2xl font-bold py-4'>Account</h1>
-      <p>User Email: {user && user.email}</p>
-
-      <button onClick={handleLogout} className='border px-6 py-2 my-4'>
-        Logout
+    <React.Fragment>
+    <style>{`
+      body {
+        background-color: #FBF9F0;
+        margin: 0;
+        padding: 0;
+      }
+    `}</style>
+    <header className='border-black border-20px h-20 py-2 flex justify-center items-center' style={{backgroundColor: '#F7F4E6'}}>
+      <img
+        className="mt-10 h-10 sm:h-12 md:h-16 lg:h-20 xl:h-40"
+        src="reby-logo.png"
+        alt="Reby's logo"
+      />
+    </header>
+    <div className='h-10 w-full pl-80 flex justify-center items-center' style={{backgroundColor: '#F7F4E6'}}>
+    </div>
+    <h1 className='text-2xl mt-10 py-4 flex justify-center' style={{fontFamily:'Raleway, sans-serif', fontWeight: 500}}>You have successfully logged in!</h1>
+    <div className='h-10 w-full mt-30 flex justify-center items-center' >
+      <button onClick={handleLogout} className='border-40px border-black-500 bg-buttonColor hover:bg-buttonHoverColor w-40 h-10 mx-auto p-2 my-2 text-white text-l rounded-md flex justify-center'
+        style={{fontFamily:'Raleway, sans-serif', fontWeight: 400}}>
+          Logout
       </button>
     </div>
+    </React.Fragment>
   );
 };
 
