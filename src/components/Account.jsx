6519231bc586fcef020991abf618279792a290b7
jocelyn-soh <110960191+jocelyn-soh@users.noreply.github.com>
2023-06-22 04:45:57 +0800

    Update Account.jsx

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { UserAuth } from '../context/AuthContext';
+import { UserAuth } from '../context/AuthContext'; 
+import Decks from './Decks';
 
 const Account = () => {
   const { user, logout } = UserAuth();
+  const [error, setError] = useState('');
   const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError('');
+    try {
+      navigate('/createdeck');
+    } catch (e) {
+      setError(e.message);
+      console.log(e.message);
+    }
+  };
 
   const handleLogout = async () => {
     try {
-      await logout(user);
+      await logout();
       navigate('/');
-      console.log('You are logged out')
+      console.log('You are logged out') 
     } catch (e) {
       console.log(e.message);
     }
@@ -25,25 +38,46 @@ const Account = () => {
         padding: 0;
       }
     `}</style>
-    <header className='border-black border-20px h-20 py-2 flex justify-center items-center' style={{backgroundColor: '#F7F4E6'}}>
+    
+    <header className='border-black border-20px h-20 py-2 flex items-center' style={{backgroundColor: '#F7F4E6'}}>
       <img
-        className="mt-10 h-10 sm:h-12 md:h-16 lg:h-20 xl:h-40"
+        className="mt-10 h-10 sm:h-12 md:h-16 lg:h-20 xl:h-40 mx-auto"
         src="reby-logo.png"
         alt="Reby's logo"
       />
+      <h1 className="font-bold text-3xl mx-auto mt-8" style={{fontFamily:'Raleway, sans-serif', fontWeight: 500}}>
+        What will you revise today, {user.displayName}?
+      </h1>
+      <div className='h-10 mt-8 mx-auto flex justify-center items-center' >
+        <button onClick={handleLogout} className='border-0px border-black-500 bg-buttonColor hover:bg-buttonHoverColor w-35 h-10 mx-auto p-2 my-2 text-white text-l rounded-md flex justify-center'
+          style={{fontFamily:'Raleway, sans-serif', fontWeight: 400}}>
+            Logout
+        </button>
+      </div>
     </header>
+
     <div className='h-10 w-full pl-80 flex justify-center items-center' style={{backgroundColor: '#F7F4E6'}}>
     </div>
-    <h1 className='text-2xl mt-10 py-4 flex justify-center' style={{fontFamily:'Raleway, sans-serif', fontWeight: 500}}>You have successfully logged in!</h1>
-    <div className='h-10 w-full mt-30 flex justify-center items-center' >
-      <button onClick={handleLogout} className='border-40px border-black-500 bg-buttonColor hover:bg-buttonHoverColor w-40 h-10 mx-auto p-2 my-2 text-white text-l rounded-md flex justify-center'
-        style={{fontFamily:'Raleway, sans-serif', fontWeight: 400}}>
-          Logout
-      </button>
+  
+    <div>
+
+      <form onSubmit={handleSubmit}>
+        <button className='text-xl border border-black hover:bg-black-700 hover:bg-buttonHoverColor focus:ring-4 focus:outline-none focus:ring-black-300 font-medium rounded-full text-sm p-0 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800 dark:hover:bg-blue-500 float-right mr-4 mt-2' 
+          style={{fontFamily:'Raleway, sans-serif', fontWeight: 400}}>
+           <h1>&nbsp; + &nbsp;</h1>
+        </button>
+
+        <h2 className="font-bold text-3xl mx-auto mt-2 ml-2" style={{fontFamily:'Raleway, sans-serif', fontWeight: 900}}>
+          My Decks 
+        </h2>
+      </form>
     </div>
+
+      <Decks />
     </React.Fragment>
   );
 };
 
 export default Account;
+
 
