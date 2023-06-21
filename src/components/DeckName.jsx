8a44b514a0843b163a1fc12b17f8e9d300df4a5b
Terri <122513546+territxl@users.noreply.github.com>
2023-06-22 05:40:22 +0800

    Update DeckName.jsx

--- src/components/DeckName.jsx
+++ src/components/DeckName.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { UserAuth } from '../context/AuthContext.js';
 import { useNavigate } from 'react-router-dom';
-import '../deck.css';
+import '../createDeck.css';
 
 const CreateDeck = ({ deckName, changeName, scrollToFlashcard, backToHome}) => {
 
